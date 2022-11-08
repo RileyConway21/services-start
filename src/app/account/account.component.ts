03ab533700df3e62049bd3267b52534380bb7fad
RileyConway21 <Rileyconway363@gmail.com>
2022-11-08 14:15:35 -0600

    init commit

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,9 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountsService} from '../accounts.service'
+
+import {LoggingService} from '../logging.service'
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  // providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
@@ -12,7 +16,8 @@ export class AccountComponent {
 
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.accountsService.updateStatus(this.id, status);
+    // this.loggingService.logStatusChanged(status)
+    this.accountsService.statusUpdated.emit(status)
   }
 }
